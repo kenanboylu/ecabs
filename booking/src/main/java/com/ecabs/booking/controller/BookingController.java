df7b817248227db6a15c93e9292e0c0afeb4c1be
kenanboylu <kenanboylu@gmail.com>
2022-11-29 21:52:04 +0300

    Added BookingController

---
+++ booking/src/main/java/com/ecabs/booking/controller/BookingController.java
@@ -0,0 +1,11 @@
+package com.ecabs.booking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "booking")
+public class BookingController {
+
+}

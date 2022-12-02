4d2a39803f818f6940d43a844efdf12594c51beb
kenanboylu <kenanboylu@gmail.com>
2022-12-02 09:49:55 +0300

    Added rabbitmq producer and consumer serivces

--- booking/src/main/java/com/ecabs/booking/service/BookingConsumerService.java
+++ booking/src/main/java/com/ecabs/booking/service/BookingConsumerService.java
@@ -1,5 +1,44 @@
 package com.ecabs.booking.service;
 
-public class BookingConsumerService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.ecabs.booking.model.Booking;
+import com.ecabs.booking.repository.BookingRepository;
+
+@Component
+public class BookingConsumerService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BookingConsumerService.class);
+	
+
+	@Autowired
+	BookingRepository repository;
+	
+	
+	@RabbitListener(queues = {"BookingAddQueue"})
+	public void addBookConsumer(Booking booking) {
+		LOGGER.info("Received message from BookingAddQueue: " + booking.toString());
+		repository.save(booking);
+	}
+	
+	@RabbitListener(queues = {"BookingEditQueue"})
+	public void editBookConsumer(Booking booking) {
+		LOGGER.info("Received message from BookingEditQueue: " + booking.toString());
+		repository.save(booking);
+	}
+	
+	@RabbitListener(queues = {"BookingDeleteQueue"})
+	public void deleteBookConsumer(Booking booking) {
+		LOGGER.info("Received message from BookingDeleteQueue: " + booking.toString());
+		repository.delete(booking);
+	}
+	
+	@RabbitListener(queues = {"MessageAuditQueue"})
+	public void auditLogConsumer(Booking booking) {
+		LOGGER.info("Received message from MessageAuditQueue: " + booking.toString());
+	}
 }
--- booking/src/main/java/com/ecabs/booking/service/BookingProducerService.java
+++ booking/src/main/java/com/ecabs/booking/service/BookingProducerService.java
@@ -1,5 +1,47 @@
 package com.ecabs.booking.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ecabs.booking.model.Booking;
+import com.ecabs.booking.repository.BookingRepository;
+
+@Component
 public class BookingProducerService {
+	
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	MessageConverter converter;
+	
+	@Autowired
+	MessageConverter messageConverter;
+	
+	@Autowired
+	BookingRepository repository;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BookingProducerService.class);
+	private static final String EXCHANGE = "message-exchange";
+	
+	
+	
+	public String sendMessage(String routingKey, Booking booking) {
+		this.rabbitTemplate.setMessageConverter(converter);
+		rabbitTemplate.convertAndSend(EXCHANGE, routingKey, booking);
+		return "message is sent!";
+	}
+	
+	public List<Booking> getAllBook() {
+		List<Booking> list = repository.findAll();
+		return list;
+	}
 
 }
